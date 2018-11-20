339abbf9ac36f44d9ddae9e5941b9c1ad15f64ec
EGOIST <0x142857@gmail.com>
2018-11-20 23:24:36 +0800

    add prettier config

--- saofile.js
+++ saofile.js
@@ -1,4 +1,4 @@
-const superb = require("superb");
+const superb = require('superb')
 const pkg = require('./package')
 
 module.exports = {
@@ -6,66 +6,66 @@ module.exports = {
   prompts() {
     return [
       {
-        name: "name",
-        message: "What is the name of the new project?",
+        name: 'name',
+        message: 'What is the name of the new project?',
         default: this.outFolder
       },
       {
-        name: "description",
-        message: "How would you descripe the new project?",
+        name: 'description',
+        message: 'How would you descripe the new project?',
         default: `my ${superb()} project`
       },
       {
-        name: "emotion",
-        message: "Use emotion (CSS-in-JS)",
-        type: "confirm",
+        name: 'emotion',
+        message: 'Use emotion (CSS-in-JS)',
+        type: 'confirm',
         default: false,
         store: true
       },
       {
-        name: "username",
-        message: "What is your GitHub username?",
+        name: 'username',
+        message: 'What is your GitHub username?',
         default: this.gitUser.username || this.gitUser.name,
         filter: v => v.toLowerCase(),
         store: true
       },
       {
-        name: "email",
-        message: "What is your GitHub email?",
+        name: 'email',
+        message: 'What is your GitHub email?',
         default: this.gitUser.email,
         store: true
       },
       {
-        name: "website",
-        message: "The URL of your website?",
+        name: 'website',
+        message: 'The URL of your website?',
         default({ username }) {
-          return `github.com/${username}`;
+          return `github.com/${username}`
         },
         store: true
       }
-    ];
+    ]
   },
   actions: [
     {
-      type: "add",
-      files: "**",
+      type: 'add',
+      files: '**',
       filters: {
-        "pages/_document.js": "emotion",
-        "pages/index-default.js": "!emotion",
-        "pages/index-emotion.js": "emotion"
+        'pages/_document.js': 'emotion',
+        'pages/index-default.js': '!emotion',
+        'pages/index-emotion.js': 'emotion'
       }
     },
     {
-      type: "move",
+      type: 'move',
       patterns: {
-        gitignore: ".gitignore",
-        "pages/index-*.js": "pages/index.js"
+        gitignore: '.gitignore',
+        'pages/index-*.js': 'pages/index.js'
       }
     }
   ],
   async completed() {
-    this.gitInit();
-    await this.npmInstall();
-    this.showProjectTips();
+    this.gitInit()
+    await this.npmInstall()
+    this.showProjectTips()
   }
-};
+}
